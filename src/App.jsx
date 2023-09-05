f36d6b6b522be94fccbd43c0262c78fc90116409
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-09-05 20:55:27 +0200

    Transform css to scss

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import Footer from "./components/Footer"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { useAtom } from 'jotai';
 import { userAtom } from './atom';
@@ -24,14 +25,19 @@ function App() {
   return (
     <>
       <BrowserRouter >
-        <Navbar />
-        <main>
+        <header>
+          <Navbar />
+        </header>
+        <main className='container'>
           <Routes>
             <Route path="/" element={ <Home />}/>
             <Route path="/register" element={ <Register />}/>
             <Route path="/login" element={ <Login />}/>
           </Routes>
         </main>
+        <footer>
+          <Footer />
+        </footer>
       </BrowserRouter>
     </>
 
