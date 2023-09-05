eeffbb5eda898a120d338db83192593f0098e878
caroline <gensac.caroline@gmail.com>
2023-09-05 18:31:37 +0200

    cards en cours

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import PropertyCard from "../components/PropertyCard";
 
 export default function Home() {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+
+    fetch('http://127.0.0.1:3000/')
+      .then(response => response.json())
+      .then(data => {
+        setProperties(data);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des propriétés :', error);
+      });
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Home</h1>
+      {properties.map((property) => (
+        <PropertyCard key={property.id} property={property} />
+      ))}
+    </div>
+  );
 }
