22ce0ed7cf70b17d28180b6ac45abd48cffa413c
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-09-06 18:26:57 +0200

    correction dans PropertyCard.jsx

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -6,12 +6,12 @@ const PropertyCard = ({ property }) => {
     <div className="card">
       {/* <img src={property.image_url} className="card-img-top" alt="property image" /> */}
       <div className="card-body">
-        <h5 className="card-title">{property.name}</h5>
+        <h5 className="card-title">{property.titre}</h5>
         <p className="card-text">{property.description}</p>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">Price: {property.price} €</li>
-        <li className="list-group-item">Location: {property.city_id}</li>
+        <li className="list-group-item">Price: {property.prix} €</li>
+        <li className="list-group-item">Location: {property.city_name}</li>
       </ul>
       <div className="card-body">
         <a href={`/properties/${property.id}`} className="card-link">See More</a>
