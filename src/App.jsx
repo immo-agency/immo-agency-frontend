de1a662e05c237d938576886d6d7a4999095fdbf
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-09-06 18:22:36 +0200

    ajout du formulaire d ajout de location

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import Register from "./pages/Register"
 import Login from "./pages/Login";
+import FormProperty from './pages/FormProperty';
 import NotFoundPage from './pages/NotFoundPage';
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Form } from "react-router-dom";
 import { useAtom } from 'jotai';
 import { userAtom } from './atom';
 import Cookies from 'js-cookie';
@@ -34,6 +35,7 @@ function App() {
             <Route exact path="/" element={ <Home />}/>
             <Route path="/register" element={ <Register />}/>
             <Route path="/login" element={ <Login />}/>
+            <Route path="/new_property" element={ <FormProperty />}/>
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
