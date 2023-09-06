32610aff652c45a3dacf54c9e51b57c96e5629d3
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-09-06 17:04:00 +0200

    ajout de la page 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import Register from "./pages/Register"
-import Login from "./pages/Login"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Footer from "./components/Footer"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login";
+import NotFoundPage from './pages/NotFoundPage';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useAtom } from 'jotai';
 import { userAtom } from './atom';
 import Cookies from 'js-cookie';
@@ -30,9 +31,10 @@ function App() {
         </header>
         <main className='container'>
           <Routes>
-            <Route path="/" element={ <Home />}/>
+            <Route exact path="/" element={ <Home />}/>
             <Route path="/register" element={ <Register />}/>
             <Route path="/login" element={ <Login />}/>
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
         <footer>
